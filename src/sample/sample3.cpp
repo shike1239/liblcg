db217fd4e77165d95c0589c32e912503587cb3a3
Yi Zhang <zhangyiss@icloud.com>
2020-05-28 21:34:02 +0800

    add sample3.cpp

---
+++ src/sample/sample3.cpp
@@ -0,0 +1,133 @@
+#include "../lib/lcg.h"
+#include "ctime"
+#include "random"
+#include "iostream"
+
+#define M 100
+#define N 80
+
+//返回范围内的随机浮点值 注意调取函数之前要调用srand(time(0));
+double random_double(double L,double T)
+{
+	return (T-L)*rand()*1.0/RAND_MAX + L;
+}
+
+//返回范围内的随机整数 注意调取函数之前要调用srand(time(0));
+int random_int(int small, int big)
+{
+	return (rand() % (big - small))+ small;
+}
+
+// 普通二维数组做核矩阵
+double **kernel;
+// 中间结果数组
+double *tmp_arr;
+
+// 计算核矩阵乘向量的乘积
+void CalAx(void* instance, const lcg_float* x, lcg_float* prod_Ax, const int n_s)
+{
+	for (int i = 0; i < M; i++)
+	{
+		tmp_arr[i] = 0.0;
+		for (int j = 0; j < n_s; j++)
+		{
+			tmp_arr[i] += kernel[i][j] * x[j];
+		}
+	}
+
+	for (int j = 0; j < n_s; j++)
+	{
+		prod_Ax[j] = 0.0;
+		for (int i = 0; i < M; i++)
+		{
+			prod_Ax[j] += kernel[i][j] * tmp_arr[i];
+		}
+	}
+	return;
+}
+
+//定义共轭梯度监控函数
+int Prog(void* instance, const lcg_float* m, const lcg_float converge, const lcg_para* param, const int n_s, const int k)
+{
+	std::clog << "Iteration-times: " << k << "\tconvergence: " << converge << std::endl;
+	if (converge > param->epsilon) std::clog << "\033[1A\033[K";
+	return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+	kernel = new double *[M];
+	for (int i = 0; i < M; i++)
+	{
+		kernel[i] = new double [N];
+	}
+
+	tmp_arr = new double [M];
+
+	srand(time(0));
+	// 添加一些大数
+	int tmp_id, tmp_size, tmp_val;
+	for (int i = 0; i < M; i++)
+	{
+		tmp_size = random_int(25, 35);
+		for (int j = 0; j < tmp_size; j++)
+		{
+			tmp_id = random_int(0, N);
+			tmp_val = random_double(-10, 10);
+
+			kernel[i][tmp_id] = tmp_val;
+		}
+	}
+
+	// 生成一组正演解
+	double *fm = new double [N];
+	for (int i = 0; i < N; i++)
+	{
+		fm[i] = random_double(1, 2);
+	}
+
+	// 计算共轭梯度B项
+	double *B = new double [N];
+	for (int i = 0; i < M; i++)
+	{
+		tmp_arr[i] = 0.0;
+		for (int j = 0; j < N; j++)
+		{
+			tmp_arr[i] += kernel[i][j]*fm[j];
+		}
+	}
+
+	for (int j = 0; j < N; j++)
+	{
+		B[j] = 0.0;
+		for (int i = 0; i < M; i++)
+		{
+			B[j] += kernel[i][j]*tmp_arr[i];
+		}
+	}
+
+	/********************准备工作完成************************/
+	lcg_para self_para = lcg_default_parameters();
+	self_para.max_iterations = 1000;
+	self_para.epsilon = 1e-10;
+
+	// 声明两组解
+	double *m = new double [N];
+	for (int i = 0; i < N; i++)
+		m[i] = 0.0;
+
+	int ret = lcg_solver(CalAx, Prog, m, B, N, &self_para, NULL, LCG_CG);
+	if (ret < 0) std::cerr << lcg_error_str(ret) << std::endl;
+
+	for (int i = 0; i < N; i++)
+	{
+		std::cout << fm[i] << " " << m[i] << std::endl;
+	}
+
+	delete[] kernel;
+	delete[] tmp_arr;
+	delete[] fm;
+	delete[] B;
+	delete[] m;
+	return 0;
+}
